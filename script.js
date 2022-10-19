9ad548fa5e63cee5eeea56473f8033e23e52a7cf
Fernandavv <112905865+Fernandavv@users.noreply.github.com>
2022-10-19 17:34:39 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function imgSlider(anything){
+    document.querySelector('.frutas').src = anything;
+}
+function CircleCor(color){
+    const Circle = document.querySelector('.circle');
+    Circle.style.background = color;
+}
