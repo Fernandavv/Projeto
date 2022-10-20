399b803bff1f62f9a6be209a943754fd7fdf7a73
Fernandavv <112905865+Fernandavv@users.noreply.github.com>
2022-10-20 16:06:37 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ function CircleCor(color){
     const Circle = document.querySelector('.circle');
     Circle.style.background = color;
 }
+
+function toggleMenu(){
+    var Menutoggle = document.querySelector('.toggle');
+    var navigation = document.querySelector('.navigation')
+    Menutoggle.classList.toggle('active')
+    navigation.classList.toggle('active')
+}
