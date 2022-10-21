dbe1e838d0f4924313d0cc834ca5438706de70c4
Fernandavv <112905865+Fernandavv@users.noreply.github.com>
2022-10-21 17:58:35 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,9 +6,55 @@ function CircleCor(color){
     Circle.style.background = color;
 }
 
+function textocor(color){
+    const texto = document.getElementsByTagName('span')[0];
+    texto.style.color = color;
+    // referencia o primeiro 'h1' do layout html
+
+}
+function button(color){
+    const button = document.querySelector('button');
+    button.style.background = color;
+    // referencia o primeiro 'h1' do layout html
+
+}
+function nav(color){
+    const nav = document.querySelector('.active');
+    nav.style.background = color;
+    // referencia o primeiro 'h1' do layout html
+
+}
+function act(color){
+    const act = document.querySelector('.act');
+    act.style.background = color;
+    // referencia o primeiro 'h1' do layout html
+
+}
+
+
+
+
+
 function toggleMenu(){
     var Menutoggle = document.querySelector('.toggle');
     var navigation = document.querySelector('.navigation')
     Menutoggle.classList.toggle('active')
     navigation.classList.toggle('active')
 }
+
+let themeswitch = document.querySelector('.themeswitch');
+let body = document.querySelector('body');
+themeswitch.classList.toggle('active');
+
+
+function funcao(imgSlider) {
+    document.getElementById("textBox").innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit.  Officia, deleniti accusamus nam dolorum quod labore  dignissimos sapiente pariatur assumenda culpa nobis commodi. Modi impedit laborum aperiam tempora autem praesentium veniam.";
+}
+function funcao2(imgSlider) {
+    document.getElementById("textBox").innerHTML = "quem ler é viadinho viadinho viadinho viadinho viadinho viadinho viadinho viadinho viadinho viadinho viadinho viadinho viadinho viadinho viadinho viadinho viadinho viadinho viadinho viadinho viadinho viadinho viadinho viadinho viadinho viadinho viadinho viadinho viadinho viadinho viadinho viadinho viadinho ";
+}
+
+function funcao3(imgSlider) {
+    document.getElementById("textBox").innerHTML = "quem ler é bigshoes bigshoes bigshoes bigshoes bigshoes bigshoes bigshoes bigshoes bigshoes bigshoes bigshoes bigshoes bigshoes bigshoes bigshoes bigshoes bigshoes bigshoes bigshoes bigshoes bigshoes bigshoes bigshoes bigshoes bigshoes bigshoes bigshoes bigshoes bigshoes bigshoes bigshoes bigshoes bigshoes bigshoes bigshoes bigshoes bigshoes bigshoes ";
+}
+
